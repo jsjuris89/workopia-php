028621de1fd9908c70c3d9677c9739bbe77092f5
Juris Poselis <jsjuris89@gmail.com>
2024-02-01 14:54:19 +0200

    added if condition for loadView, loadPartial to throw meaningful error in browser

--- helpers.php
+++ helpers.php
@@ -17,7 +17,14 @@
  * @return void
  */
 function loadView($name) {
-   require basePath("views/{$name}.view.php");
+
+   $viewPath = basePath("views/{$name}.view.php");
+
+   if(file_exists($viewPath)) {
+      require $viewPath;
+   } else {
+      echo "View '{$name}' not found!";
+   }
 }
 
 
@@ -28,6 +35,12 @@ function loadView($name) {
  * @return void
  */
 function loadPartial($name) {
-   require basePath("views/partials/{$name}.php");
+   $partialPath = basePath("views/partials/{$name}.php");
+
+   if(file_exists($partialPath)) {
+      require $partialPath;
+   } else {
+      echo "Partial '{$name}' not found";
+   }
 }
 
