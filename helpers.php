7fe29bb30f341b0fce2286ba7ac4045535c5b87f
Juris Poselis <jsjuris89@gmail.com>
2024-02-01 14:17:44 +0200

    created helper loadView()

--- helpers.php
+++ helpers.php
@@ -6,7 +6,16 @@
  * @param string $path
  * @return string
  */
-
  function basePath($path = '') {
     return __DIR__ . '/' . $path;
  }
+
+/**
+ * Load a view
+ * 
+ * @param string $name
+ * @return void
+ */
+function loadView($name) {
+   require basePath("views/${name}.view.php");
+}
